753db521d702ca559efb969b8a95cc40e70fa486
Ike Odira <odiraike8@gmail.com>
2024-07-23 22:41:40 +0100

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+  res.json({
+    message: "User Api route is working",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -17,3 +18,11 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is runnning on port 3000!!");
 });
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "Hello World",
+//   });
+// });
+
+app.use("/api/user", userRouter);
